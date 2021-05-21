9825abe9e2b2fbfb04c1780b83f123c19902d926
Joel Kundu <62535351+JOELKUNDU@users.noreply.github.com>
2021-05-21 23:18:12 +0530

    Add files via upload

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,8 +1,10 @@
+#! /bin/sh
 pip install flask 
 pip install flask-sqlalchemy
 pip install flask-wtf
 pip install wtforms
 pip install pyfladesk
 pip install sklearn
-pip install tensorflow
+pip install numpy
+pip install pandas
 pip install joblib
